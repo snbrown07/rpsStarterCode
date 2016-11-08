e06f6352b77b96a00466ae672a6db5673b9c4920
snbrown07 <sbrow5081@withs.org>
2016-11-08 19:25:38 +0000

    completed homework

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,14 +1,17 @@
 // Task 1: Create a function that randomly chooses among 'rock', 'paper', or 'scissors'.
-var generateComputerChoice = function () {
+var generateComputerChoice = function (r, p, s) {
     // Task 1, Step 1: Create an array with three elements ("rock", "paper" and "scissors").
-
+    var choices = ["rock", "paper", "scissors" ];
     // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
     // number between 0 and 2. Be sure to save it to a variable.
-
+    var randomChoice = Math.floor(Math.random() * 3);
+    console.log(randomChoice);
     // Task 1, Step 3: Use this randomly generated number to pull a value from the
     // array (eg myArray[randomNum])
+    var answer = choices[randomChoice];
 
     // Task 1, Step 4: return this new value
+    return answer;
 };
 
 // Task 2: Create a function that compares the userChoice and the computerChoice
@@ -18,12 +21,33 @@ var pickWinner = function (userChoice, computerChoice) {
     console.log("computer choice: " + computerChoice);
     // Task 2, Step 1: Create an "if/else if/else" statement that compares the
     // userChoice and computerChoice under any possible game outcome.
-
+    var winner; 
+    if (computerChoice === "rock" && userChoice === "rock"){
+        winner = "Draw";
+    }  else if (computerChoice === "rock" && userChoice === "paper"){
+        winner = "User Wins";
+    } else if (computerChoice === "paper" && userChoice === "rock"){
+        winner = "Computer Wins";
+    } else if (computerChoice === "paper" && userChoice === "paper"){
+        winner = "Draw";
+    }  else if (computerChoice === "paper" && userChoice === "scissors"){
+        winner = "User Wins";
+    } else if (computerChoice === "scissors" && userChoice === "paper"){
+        winner = "Computer Wins";
+    } else if (computerChoice === "scissors" && userChoice === "scissors"){
+        winner = "Draw";
+    }  else if (computerChoice === "scissors" && userChoice === "rock"){
+        winner = "User Wins";
+    } else if (computerChoice === "rock" && userChoice === "scissors"){
+        winner = "Computer Wins";
+    }
     // Task 2, Step 2: Depending on who is the winner of the game console.log
     // either "user wins", "computer wins" or "draw"
-
+    console.log(winner);
     // Task 4: Show `computerChoice` in HTML after the words "Computer's choice:"
+    $("#computer-choice").html(computerChoice);
     // Task 5: Show the winner in HTML after the word "Winner:"
+    $("#winner").html(winner);
 };
 
 /* DOCUMENT READY: Everything inside this function will happen after
